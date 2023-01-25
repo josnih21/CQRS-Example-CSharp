30ac4babab2cd1c0152f35038655ee373885c583
´josnih´ <josnih@gmail.com>
2023-01-25 15:42:18 +0000

    :sparkles: Add sync controller to update write model and read model on start-up

---
+++ Api/Controllers/SyncController.cs
@@ -0,0 +1,20 @@
+﻿using CQRS.EventHandler;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+/// <summary>
+/// This class aims to sync the Write Model with the Read Model normaly this will be a separataly program or solution that
+/// allows to synchronize the models on each startup. 
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class SyncController : ControllerBase
+{
+    private readonly UserSynchronizer _userSynchronizer = new UserSynchronizer();
+
+    [HttpPost]
+    public void Post()
+    {
+        _userSynchronizer.syncAll();
+    }
+}
